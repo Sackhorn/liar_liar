e98c6312314d68f089a6b281b049e6c0cefa0585
sackhornxd <sackhorn@gmail.com>
2019-05-05 00:33:42 +0200

    added checkpoints callback, removed loss logging

--- models/BaseModels/SequentialModel.py
+++ models/BaseModels/SequentialModel.py
@@ -3,7 +3,7 @@ from typing import List, Any
 import tensorflow as tf
 from tensorflow.python import keras
 from tensorflow.python.keras import Model
-from tensorflow.python.keras.callbacks import TensorBoard, ReduceLROnPlateau
+from tensorflow.python.keras.callbacks import TensorBoard, ReduceLROnPlateau, ModelCheckpoint
 
 from models.BaseModels.DataProvider import DataProvider
 from matplotlib import pyplot as plt
@@ -39,19 +39,11 @@ class SequentialModel(Model, DataProvider):
         shape[-1] = self.num_classes
         return tf.TensorShape(shape)
 
-    def train(self, epochs=1, train_data=None, callbacks=None):
-        class LossHistory(tf.keras.callbacks.Callback):
-            def on_train_begin(self, logs={}):
-                self.losses = []
-
-            def on_batch_end(self, batch, logs={}):
-                self.losses.append(logs.get('loss'))
-
-        history = LossHistory()
+    def train(self, epochs=1, train_data=None, callbacks=[]):
         tsboard = TensorBoard(log_dir=self.get_tensorboard_path(), histogram_freq=10, write_graph=True)
+        checkpoint = ModelCheckpoint(self.SAVE_DIR, save_best_only=True, save_weights_only=True)
         lr_callback = ReduceLROnPlateau(factor=0.5, patience=10, verbose=1, min_delta=0.005)
-        callback = [history, tsboard, lr_callback]
-        callback = callback if callbacks is None else callback + callbacks
+        callback = [tsboard, lr_callback, checkpoint] + callbacks
         train = self.train_dataset if train_data is None else train_data
         self.fit(train.repeat(),
                  epochs=epochs,
@@ -60,15 +52,11 @@ class SequentialModel(Model, DataProvider):
                  validation_data=self.test_dataset,
                  validation_steps=self.test_steps)
 
-        self.evaluate(self.test_dataset,  steps=self.test_steps)
-        # self.evaluate(self.train_dataset,  steps=self.train_steps_per_epoch)
+        self.evaluate(self.test_dataset, steps=self.test_steps)
         self.save_model_data()
-        plt.plot(history.losses)
-        plt.show()
 
     def get_dataset(self, split, name='', batch_size=32, shuffle=10000, nmb_classes=10):
         dataset =  super().get_dataset(split, name, batch_size, shuffle, nmb_classes)
-        # dataset = dataset.map(lambda x,y: (tf.divide(x, 255), y))
         return dataset
 
     def test(self, test_data=None):
