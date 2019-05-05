63ef111fc5f99a59ba899dab996944714d43a4da
sackhornxd <sackhorn@gmail.com>
2019-05-05 12:56:31 +0200

    removed hardcoded values from attacks

--- models/Attacks/deepfool.py
+++ models/Attacks/deepfool.py
@@ -1,6 +1,7 @@
 from tensorflow.python import enable_eager_execution
 from models.utils.images import show_plot
 from models.MNISTModels.DenseModel import DenseModel
+from models.BaseModels.SequentialModel import SequentialModel
 
 import numpy as np
 import tensorflow as tf
@@ -12,6 +13,12 @@ import tensorflow_datasets as tfds
 
 
 def deepfool(data_sample, model, max_iter=100, min=0.0, max=1.0):
+    """
+
+    :type model: SequentialModel
+    """
+
+    nmb_classes = model.get_number_of_classes()
     image, label = data_sample
     label = tf.argmax(tf.squeeze(label)).numpy()
     show_plot(model(image), image)
@@ -22,13 +29,13 @@ def deepfool(data_sample, model, max_iter=100, min=0.0, max=1.0):
         with tf.GradientTape(persistent=True) as tape:
             tape.watch(image)
             logits = model(image, get_raw=True)
-            for k in range(10):
+            for k in range(nmb_classes):
                 gradients_by_cls.append(tf.squeeze(tape.gradient(logits[0][k], image)).numpy())
 
         logits = tf.squeeze(logits).numpy()
         w_prime = []
         f_prime = []
-        for k in range(10):
+        for k in range(nmb_classes):
             if k == tf.squeeze(label).numpy():
                 f_prime.append(float('+inf'))
                 w_prime.append(float("+inf"))
@@ -36,14 +43,14 @@ def deepfool(data_sample, model, max_iter=100, min=0.0, max=1.0):
             w_prime.append(gradients_by_cls[k] - gradients_by_cls[label])
             f_prime.append(logits[k] - logits[label])
         tmp = []
-        for k in range(10):
+        for k in range(nmb_classes):
             if k == tf.squeeze(label).numpy():
                 tmp.append(float("+inf"))
                 continue
             tmp.append(abs(f_prime[k]) / np.linalg.norm(w_prime[k]))
         l = np.argmin(tmp)
         perturbation = (abs(f_prime[l]) * w_prime[l]) / np.square(np.linalg.norm(w_prime[l]))
-        image = image + tf.reshape(tf.convert_to_tensor(perturbation, dtype=tf.float32), (1, 28, 28, 1))
+        image = image + tf.reshape(tf.convert_to_tensor(perturbation, dtype=tf.float32), model.get_input_shape())
         image = tf.clip_by_value(image, min, max)
         iter += 1
     show_plot(model(image), image)
--- models/Attacks/jsma.py
+++ models/Attacks/jsma.py
@@ -1,5 +1,6 @@
 from tensorflow.python import enable_eager_execution, Tensor
-from models.MNISTModels.ConvModel import ConvModel
+from models.CIFAR10Models.ConvModel import ConvModel
+from models.MNISTModels.DenseModel import DenseModel
 from models.utils.images import show_plot
 import numpy as np
 import tensorflow as tf
@@ -10,23 +11,31 @@ import tensorflow_datasets as tfds
 # Source https://arxiv.org/pdf/1511.07528.pdf
 
 
-def jsma(data_sample, model, i, eps, y_target=None, min=0.0, max=1.0):
+def jsma(data_sample, model, i, eps, min=0.0, max=1.0):
     image, label = data_sample
+    label = tf.argmax(tf.squeeze(label)).numpy()
     show_plot(model(image), image)
-    all_pixels = generate_all_pixels(tf.squeeze(image).shape)
+    input_shape = model.get_input_shape()
+    all_pixels = generate_all_pixels(input_shape)
     iter = 0
     while iter < i and len(all_pixels) > 0:
-        all_pixel_pairs = generate_all_pixel_pairs(all_pixels)
-        chosen_pixel_pair = saliency_map(model, image, label, all_pixel_pairs)
-        add_tensor = np.zeros((28,28))
+        chosen_pixel_pair = saliency_map(model, image, label, all_pixels)
+        add_tensor = np.zeros(input_shape)
         for pixel in chosen_pixel_pair:
-            add_tensor[pixel[0]][pixel[1]] = 0.6
-            pixel_val = image.numpy()[0][pixel[0]][pixel[1]]
+
+            if input_shape[2] > 1:
+                add_tensor[pixel[0]][pixel[1]][pixel[2]] = eps
+                pixel_val = image.numpy()[0][pixel[0]][pixel[1]][pixel[2]]
+            else:
+                add_tensor[pixel[0]][pixel[1]] = eps
+                pixel_val = image.numpy()[0][pixel[0]][pixel[1]]
+
             if pixel_val < 1e-10 or pixel_val > 0.99:
                 all_pixels.remove(pixel)
-        add_tensor = add_tensor.reshape((1,28,28,1))
+
+        add_tensor = add_tensor.reshape(input_shape)
         add_tensor = tf.convert_to_tensor(add_tensor, dtype=tf.float32)
-        tf.reshape(add_tensor, (1,28,28,1))
+        tf.reshape(add_tensor, input_shape)
         image = image + add_tensor
         image = tf.clip_by_value(image, min, max)
         show_plot(model(image), image)
@@ -34,38 +43,57 @@ def jsma(data_sample, model, i, eps, y_target=None, min=0.0, max=1.0):
         iter += 1
     show_plot(model(image), image)
 
-def saliency_map(model, image, label, all_pixel_pairs):
-    target_label = 9
-    # true_label = tf.argmax(tf.squeeze(label)).numpy()
+# TODO:nie traktować każdej barwy jako osobny ficzer tylko sumować i zmieniać intensywność
+def saliency_map(model, image, true_label, all_pixels):
+    nmb_classes = model.get_number_of_classes()
+    input_shape = model.get_input_shape()
     grds_by_cls = []
     with tf.GradientTape(persistent=True) as tape:
         tape.watch(image)
         logits = model(image)
-        for k in range(10):
+        for k in range(nmb_classes):
             grds_by_cls.append(tf.squeeze(tape.gradient(logits[0][k], image)).numpy())
+
     max = float("-inf")
     return_pixel_pair = None
-    for pair in all_pixel_pairs:
-        alpha = 0.0
-        beta = 0.0
-        for pixel in pair:
-            alpha += grds_by_cls[target_label][pixel[0]][pixel[1]]
-            for class_nmb in range(10):
-                if class_nmb != target_label:
-                    beta += grds_by_cls[class_nmb][pixel[0]][pixel[1]]
-        if alpha > 0.0 and beta < 0 and -alpha*beta > max:
-            return_pixel_pair = pair
-            max = -alpha*beta
+
+
+    for i in range(len(all_pixels)):
+        first = all_pixels[i]
+        for j in all_pixels[i+1:]:
+            second = j
+            pixel_pair = (first, second)
+            alpha = 0.0
+            beta = 0.0
+            for pixel in pixel_pair:
+
+                if input_shape[2] > 1:
+                    alpha += grds_by_cls[true_label][pixel[0]][pixel[1]][pixel[2]]
+                else:
+                    alpha += grds_by_cls[true_label][pixel[0]][pixel[1]]
+
+                for class_nmb in range(nmb_classes):
+                    if class_nmb != true_label:
+                        if input_shape[2] > 1:
+                            beta += grds_by_cls[class_nmb][pixel[0]][pixel[1]][pixel[2]]
+                        else:
+                            beta += grds_by_cls[class_nmb][pixel[0]][pixel[1]]
+
+            if alpha > 0.0 and beta < 0 and -alpha*beta > max:
+                return_pixel_pair = pixel_pair
+                max = -alpha*beta
+
     return return_pixel_pair
 
 
 
 def generate_all_pixels(shape):
-    x, y = shape
+    x, y, z = shape
     all_pixels = []
     for i in range(x):
         for j in range(y):
-            all_pixels.append((i, j))
+            for k in range(z):
+                all_pixels.append((i, j, k))
     return all_pixels
 
 def generate_all_pixel_pairs(all_pixels):
@@ -78,15 +106,16 @@ def generate_all_pixel_pairs(all_pixels):
     return all_pairs
 
 
-def execute_jsma(data_sample, model, i, eps, min=0.0, max=1.0):
+def execute_jsma(data_sample, model, i, eps,  min=0.0, max=1.0):
     return jsma(data_sample, model, i, eps, min=min, max=max)
 
 def test_fgsm_mnist():
     model = ConvModel()
+    # model = DenseModel()
     model.load_model_data()
     eval_dataset = model.get_dataset(tfds.Split.TEST, batch_size=1)
     for data_sample in eval_dataset.take(1):
-        execute_jsma(data_sample, model, 80, 0.0001)
+        execute_jsma(data_sample, model, 25, 1.0)
 
 if __name__ == "__main__":
     enable_eager_execution()
