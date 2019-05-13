471ad914f2f0c5b4d253bb56ef12b95a661a1838
sackhornxd <sackhorn@gmail.com>
2019-05-13 06:02:53 +0200

    added normal distribution and Gaussian operator

--- models/Attacks/evolutionary_attack.py
+++ models/Attacks/evolutionary_attack.py
@@ -15,10 +15,11 @@ import tensorflow as tf
 # https://arxiv.org/pdf/1412.1897.pdf
 # https://arxiv.org/pdf/1504.04909.pdf - tutaj jest opisana strategia ewolucyjna
 # https://pdfs.semanticscholar.org/3461/99d0c8fce4bd91be855155bb1fe71844e9ac.pdf - polynomial mutation operator
+from models.MNISTModels.LeNet5 import LeNet5
 from models.utils.images import show_plot
 
 
-def evolutionary_attack(model, population_size, iter_max, min=0.0, max=1.0, mutation_chance=0.1, mutation_power=15):
+def evolutionary_attack(model, population_size, iter_max, min=0.0, max=1.0, mutation_chance=0.1, mutation_power=20, use_normal=False, use_gausian=False):
     """
     :type model: SequentialModel
     """
@@ -26,22 +27,30 @@ def evolutionary_attack(model, population_size, iter_max, min=0.0, max=1.0, muta
     best_in_class = {} #dictionary of specimen performing best for given class
 
     for x in range(population_size):
-        population.append(np.random.uniform(min, max, model.get_input_shape())) #initialize specimens with random values
+        if use_normal:
+            population.append(np.random.normal(0.0, 0.3, model.get_input_shape())) #initialize specimens with random values
+        else:
+            population.append(np.random.uniform(min, max, model.get_input_shape())) #initialize specimens with random values
 
     for i in range(iter_max):
         if i%100==0: print(i)
         current_specimen = random.choice(population)
-        mutated_specimen = mutate_specimen(current_specimen, min, max, mutation_chance, mutation_power)
-        logits = model(tf.expand_dims(tf.convert_to_tensor(mutated_specimen, dtype=tf.float32), 0), get_raw=True)
-        best_class = tf.argmax(tf.squeeze(logits)).numpy()
-        certainty = tf.reduce_max(tf.squeeze(logits)).numpy()
-        if best_class in best_in_class:
-            _, best_certainty = best_in_class[best_class]
-            if certainty > best_certainty:
-                best_in_class[best_class] = (mutated_specimen, certainty)
-                population.append(mutated_specimen)
+        if use_gausian:
+            mutated_specimen = mutate_specimen_gausian(current_specimen, min, max, mutation_chance, mutation_power)
         else:
-            best_in_class[best_class] = (mutated_specimen, certainty)
+            mutated_specimen = mutate_specimen(current_specimen, min, max, mutation_chance, mutation_power)
+        logits = model(tf.expand_dims(tf.convert_to_tensor(mutated_specimen, dtype=tf.float32), 0))
+
+        certainties_per_class = tf.squeeze(logits).numpy()
+
+        for cur_class in range(len(certainties_per_class)):
+            if cur_class in best_in_class:
+                _, best_certainty = best_in_class[cur_class]
+                if certainties_per_class[cur_class] > best_certainty:
+                    best_in_class[cur_class] = (mutated_specimen, certainties_per_class[cur_class])
+                    population.append(mutated_specimen)
+            else:
+                best_in_class[cur_class] = (mutated_specimen, certainties_per_class[cur_class])
 
     for _, specimen_tuple in best_in_class.items():
         specimen, _ = specimen_tuple
@@ -64,30 +73,23 @@ def mutate_specimen(specimen, min, max, mutation_chance, mutation_power=15):
                 old_val[...] = new_val
     return specimen
 
+def mutate_specimen_gausian(specimen, min, max, mutation_chance, mutation_power=15):
+    specimen = np.copy(specimen)
+    std_dev = 0.5 * (max-min)
+    with np.nditer(specimen, op_flags=['readwrite']) as it:
+        for old_val in it:
+            if np.random.uniform() < mutation_chance:
+                new_val = np.random.normal(old_val, std_dev)
+                old_val[...] = np.clip(new_val, min, max)
+    return specimen
+
 
 def test_ea_attack():
     model = DenseModel()
     model.load_model_data()
     model.test()
-    # for i in range(10):
-    #     numpy = np.random.uniform(low=0.0, high=1.0, size=model.get_input_shape())
-    #     plt.imshow(np.squeeze(numpy), cmap='gray')
-    #     plt.show()
-    #     tensor = tf.expand_dims(tf.convert_to_tensor(numpy, dtype=tf.float32), 0)
-    #     logits = model(tensor, get_raw=True)
-    #     show_plot(logits, tensor)
-    evolutionary_attack(model, 1000, 10000, mutation_chance=0.5)
+    evolutionary_attack(model, 100, 2000, use_normal=True, use_gausian=False)
 
-    # dict = {}
-    # for datasample in model.get_dataset(Split.TRAIN, batch_size=1):
-    #     image, label = datasample
-    #     numb = tf.argmax(tf.squeeze(label)).numpy().astype(int)
-    #     if numb not in dict:
-    #         dict[numb] = 1
-    #     else:
-    #         dict[numb] = dict[numb] + 1
-    #
-    # print (dict)
 
 if __name__ == "__main__":
     enable_eager_execution()
