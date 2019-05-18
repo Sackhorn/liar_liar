68fcfbf9aa3ddeefc0e4ba7ec615ddef15650a06
sackhornxd <sackhorn@gmail.com>
2019-05-18 20:37:41 +0200

    fixed bfgs

--- models/Attacks/bfgs.py
+++ models/Attacks/bfgs.py
@@ -28,22 +28,26 @@ def bfgs(data_sample, model, i_max, target_label, min=0.0, max=1.0):
         input_np_arr = image.reshape(model.get_input_shape()) + perturbation.reshape(model.get_input_shape())
         input_tensor = tf.convert_to_tensor(input_np_arr, dtype=tf.float32)
         input_tensor = tf.expand_dims(input_tensor, 0)
-        return c * np.linalg.norm(perturbation) + sparse_softmax_cross_entropy(target_label, model(input_tensor))
+        return c * np.linalg.norm(perturbation) + categorical_crossentropy(target_label, model(input_tensor))
 
-    c = 0.0001
-    while True:
+
+    bounds = [(-image[i], max - image[i]) for i in range(len(image))]
+    c = 0.01
+    for i in range(30):
         new_perturbation, min_func_val, ret_dict = fmin_l_bfgs_b(min_func,
                                                                  perturbation,
                                                                  args=(c, image),
                                                                  approx_grad=True,
                                                                  maxiter=i_max,
-                                                                 epsilon=0.01)
+                                                                 bounds=bounds,
+                                                                 m=15,
+                                                                 epsilon=1e-5)
         # print(ret_dict['grad'])
         input_np_arr = image.reshape(model.get_input_shape()) + new_perturbation.reshape(model.get_input_shape())
         input_tensor = tf.convert_to_tensor(input_np_arr, dtype=tf.float32)
         input_tensor = tf.expand_dims(input_tensor, 0)
         show_plot(model(input_tensor), input_np_arr, model.get_label_names())
-        if tf.squeeze(model(input_tensor)).numpy().argmax() == tf.squeeze(target_label).numpy():
+        if tf.squeeze(model(input_tensor)).numpy().argmax() == tf.argmax(tf.squeeze(target_label)).numpy():
             print("max c= " + str(c))
             break
         else:
@@ -53,24 +57,26 @@ def bfgs(data_sample, model, i_max, target_label, min=0.0, max=1.0):
     c_high = c
     c_low = 0.0
 
-    while c_high - c_low > 0.0001:
+    while c_high - c_low >= 0.01:
         c_half = (c_high + c_low)/2
         new_perturbation, min_func_val, ret_dict = fmin_l_bfgs_b(min_func,
                                                                  perturbation,
                                                                  args=(c_half, image),
                                                                  approx_grad=True,
                                                                  maxiter=i_max,
-                                                                 epsilon=0.01)
+                                                                 bounds=bounds,
+                                                                 m=15,
+                                                                 epsilon=1e-5)
         # print(ret_dict['grad'])
         input_np_arr = image.reshape(model.get_input_shape()) + new_perturbation.reshape(model.get_input_shape())
         input_tensor = tf.convert_to_tensor(input_np_arr, dtype=tf.float32)
         input_tensor = tf.expand_dims(input_tensor, 0)
         show_plot(model(input_tensor), input_np_arr, model.get_label_names())
-        if tf.squeeze(model(input_tensor)).numpy().argmax() == tf.squeeze(target_label).numpy():
+        if tf.squeeze(model(input_tensor)).numpy().argmax() == tf.argmax(tf.squeeze(target_label)).numpy():
             print("tested halfing c= " + str(c_half))
             c_high = c_half
         else:
-            print("tested halfing c= " + str(c_half))
+            print("ending halfing c= " + str(c_half))
             c_low = c_half
 
 
@@ -84,11 +90,11 @@ def bfgs(data_sample, model, i_max, target_label, min=0.0, max=1.0):
 def test_fgsm_mnist():
     model = ConvModel()
     model.load_model_data()
-    target_label = tf.constant(6, dtype=tf.int64, shape=(1))
+    target_label = tf.one_hot(tf.constant(6, dtype=tf.int64, shape=(1)), model.get_number_of_classes())
     for data_sample in  model.get_dataset(Split.TEST, batch_size=1).take(1):
         bfgs(data_sample, model, 10, target_label)
 
 if __name__ == "__main__":
     enable_eager_execution()
-    for i in range(3):
+    for i in range(1):
         test_fgsm_mnist()
