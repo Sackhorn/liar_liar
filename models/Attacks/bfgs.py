f2fa2103681d3a073e4e1c2b3478b1cc6b3c4ad7
sackhornxd <sackhorn@gmail.com>
2019-05-19 20:53:25 +0200

    added gradient computation to bfgs
    works much faster now

--- models/Attacks/bfgs.py
+++ models/Attacks/bfgs.py
@@ -22,30 +22,45 @@ def bfgs(data_sample, model, i_max, target_label, min=0.0, max=1.0):
     image, label = data_sample
 
     image = image.numpy().flatten()
-    perturbation = np.random.uniform(min, max/100, model.get_input_shape()).flatten().astype(dtype=np.float32)
+
+    perturbation = [np.random.uniform(-image[i]/100, (max - image[i])/100) for i in range(len(image))]
+    perturbation = np.array(perturbation)
+    # perturbation = np.random.uniform(min, max/50, model.get_input_shape()).flatten().astype(dtype=np.float32)
+
+
     input_np_arr = image.reshape(model.get_input_shape()) + perturbation.reshape(model.get_input_shape())
     input_tensor = tf.convert_to_tensor(input_np_arr, dtype=tf.float32)
     input_tensor = tf.expand_dims(input_tensor, 0)
     show_plot(model(input_tensor), input_np_arr, model.get_label_names())
 
     def min_func(perturbation, c, image):
-        input_np_arr = image.reshape(model.get_input_shape()) + perturbation.reshape(model.get_input_shape())
-        input_tensor = tf.convert_to_tensor(input_np_arr, dtype=tf.float32)
-        input_tensor = tf.expand_dims(input_tensor, 0)
-        return c * np.linalg.norm(perturbation) + categorical_crossentropy(target_label, model(input_tensor))
+        image = image.reshape(model.get_input_shape())
+        image = tf.convert_to_tensor(image, dtype=tf.float32)
+
+        perturbation = perturbation.reshape(model.get_input_shape())
+        perturbation = tf.convert_to_tensor(perturbation, dtype=tf.float32)
+
+
+
+        with tf.GradientTape() as tape:
+            tape.watch(perturbation)
+            input_tensor = tf.add(image, perturbation)
+            input_tensor = tf.expand_dims(input_tensor, 0)
+            value = tf.add(tf.multiply(c, tf.linalg.norm(perturbation)),
+                           categorical_crossentropy(target_label, model(input_tensor)))
+        gradient = tape.gradient(value, perturbation)
+
+        return value.numpy(), gradient.numpy().flatten().astype(np.float64)
 
 
     bounds = [(-image[i], max - image[i]) for i in range(len(image))]
-    c = 1e-5
-    for i in range(30):
+    c = 1
+    for i in range(10):
         new_perturbation, min_func_val, ret_dict = fmin_l_bfgs_b(min_func,
                                                                  perturbation,
                                                                  args=(c, image),
-                                                                 approx_grad=True,
                                                                  maxiter=i_max,
-                                                                 bounds=bounds,
-                                                                 m=15,
-                                                                 epsilon=1e-5)
+                                                                 bounds=bounds)
         # print(ret_dict['grad'])
         input_np_arr = image.reshape(model.get_input_shape()) + new_perturbation.reshape(model.get_input_shape())
         input_tensor = tf.convert_to_tensor(input_np_arr, dtype=tf.float32)
@@ -61,16 +76,13 @@ def bfgs(data_sample, model, i_max, target_label, min=0.0, max=1.0):
     c_high = c
     c_low = 0.0
 
-    while c_high - c_low >= 1e-5:
+    while c_high - c_low >= 1:
         c_half = (c_high + c_low)/2
         new_perturbation, min_func_val, ret_dict = fmin_l_bfgs_b(min_func,
                                                                  perturbation,
                                                                  args=(c_half, image),
-                                                                 approx_grad=True,
                                                                  maxiter=i_max,
-                                                                 bounds=bounds,
-                                                                 m=15,
-                                                                 epsilon=1e-5)
+                                                                 bounds=bounds)
         # print(ret_dict['grad'])
         input_np_arr = image.reshape(model.get_input_shape()) + new_perturbation.reshape(model.get_input_shape())
         input_tensor = tf.convert_to_tensor(input_np_arr, dtype=tf.float32)
@@ -96,7 +108,7 @@ def test_fgsm_mnist():
     model.load_model_data()
     target_label = tf.one_hot(tf.constant(6, dtype=tf.int64, shape=(1)), model.get_number_of_classes())
     for data_sample in  model.get_dataset(Split.TEST, batch_size=1).take(1):
-        bfgs(data_sample, model, 100, target_label)
+        bfgs(data_sample, model, 10000, target_label)
 
 if __name__ == "__main__":
     enable_eager_execution()
