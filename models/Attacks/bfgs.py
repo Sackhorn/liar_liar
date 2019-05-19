378a319c0d0d09bccac331ba8b93cfe444163a49
sackhornxd <sackhorn@gmail.com>
2019-05-19 19:27:45 +0200

    changed initial perturbation to image to 1/100th
    changed eps value
    and binary search

--- models/Attacks/bfgs.py
+++ models/Attacks/bfgs.py
@@ -22,7 +22,11 @@ def bfgs(data_sample, model, i_max, target_label, min=0.0, max=1.0):
     image, label = data_sample
 
     image = image.numpy().flatten()
-    perturbation = np.random.uniform(min, max, model.get_input_shape()).flatten().astype(dtype=np.float32)
+    perturbation = np.random.uniform(min, max/100, model.get_input_shape()).flatten().astype(dtype=np.float32)
+    input_np_arr = image.reshape(model.get_input_shape()) + perturbation.reshape(model.get_input_shape())
+    input_tensor = tf.convert_to_tensor(input_np_arr, dtype=tf.float32)
+    input_tensor = tf.expand_dims(input_tensor, 0)
+    show_plot(model(input_tensor), input_np_arr, model.get_label_names())
 
     def min_func(perturbation, c, image):
         input_np_arr = image.reshape(model.get_input_shape()) + perturbation.reshape(model.get_input_shape())
@@ -32,7 +36,7 @@ def bfgs(data_sample, model, i_max, target_label, min=0.0, max=1.0):
 
 
     bounds = [(-image[i], max - image[i]) for i in range(len(image))]
-    c = 0.01
+    c = 1e-5
     for i in range(30):
         new_perturbation, min_func_val, ret_dict = fmin_l_bfgs_b(min_func,
                                                                  perturbation,
@@ -52,12 +56,12 @@ def bfgs(data_sample, model, i_max, target_label, min=0.0, max=1.0):
             break
         else:
             print("tested incerasing c= " + str(c))
-            c = 2*c
+            c = c*2
 
     c_high = c
     c_low = 0.0
 
-    while c_high - c_low >= 0.01:
+    while c_high - c_low >= 1e-5:
         c_half = (c_high + c_low)/2
         new_perturbation, min_func_val, ret_dict = fmin_l_bfgs_b(min_func,
                                                                  perturbation,
@@ -76,7 +80,7 @@ def bfgs(data_sample, model, i_max, target_label, min=0.0, max=1.0):
             print("tested halfing c= " + str(c_half))
             c_high = c_half
         else:
-            print("ending halfing c= " + str(c_half))
+            print("tested halfing c= " + str(c_half))
             c_low = c_half
 
 
@@ -92,7 +96,7 @@ def test_fgsm_mnist():
     model.load_model_data()
     target_label = tf.one_hot(tf.constant(6, dtype=tf.int64, shape=(1)), model.get_number_of_classes())
     for data_sample in  model.get_dataset(Split.TEST, batch_size=1).take(1):
-        bfgs(data_sample, model, 10, target_label)
+        bfgs(data_sample, model, 100, target_label)
 
 if __name__ == "__main__":
     enable_eager_execution()
