dd983267195614d05189e534fcb3cc80c36e5e81
sackhornxd <sackhorn@gmail.com>
2019-05-20 03:30:52 +0200

    multiple changes to fix c_and_w

--- models/Attacks/c_and_w.py
+++ models/Attacks/c_and_w.py
@@ -2,13 +2,12 @@
 
 from scipy.optimize import fmin_l_bfgs_b
 from tensorflow.python import enable_eager_execution
-from tensorflow.python.keras.losses import categorical_crossentropy, sparse_categorical_crossentropy
-from tensorflow.python.ops.losses.losses_impl import sparse_softmax_cross_entropy
 from tensorflow.python.training.adam import AdamOptimizer
 from tensorflow_datasets import Split
 
-from models.CIFAR10Models.ConvModel import ConvModel
+from models.MNISTModels.ConvModel import ConvModel
 from models.MNISTModels.DenseModel import DenseModel
+from models.MNISTModels.LeNet5 import LeNet5
 from models.utils.images import show_plot
 from models.BaseModels.SequentialModel import SequentialModel
 
@@ -22,73 +21,57 @@ def optimize_c_and_w(data_sample, model, i_max, target_label, c_val, min=0.0, ma
     """
 
     image, label = data_sample
-    perturbation = [np.random.uniform(-image[i], (max - image[i])) for i in range(len(image))]
-    perturbation = np.array(perturbation)
-    perturbation = perturbation.reshape(model.get_input_shape())
-    perturbation = tf.convert_to_tensor(perturbation, dtype=tf.float32)
-    perturbation = tf.Variable(perturbation)
+
+    perturbation = tf.Variable(np.random.normal(min, max, image.shape), dtype=tf.float32)
 
     def loss():
-        softmax = tf.squeeze(model(image+perturbation))
-        highest_prob = tf.argmax(softmax)
-        first = tf.concat([softmax[:highest_prob], softmax[highest_prob+1:]], 0) #get all elements except of the one with highest probability
+        delta = 0.5 * (tf.tanh(perturbation) + 1)
+        softmax = tf.squeeze(model(delta))
+        target_label_index = target_label.numpy().squeeze().argmax()
+        first = tf.concat([softmax[:target_label_index], softmax[target_label_index+1:]], 0) #get all elements except of the one with highest probability
         first = tf.reduce_max(first)
-        second = softmax[target_label.numpy().squeeze().argmax()]
-        g_func = tf.maximum(first - second, tf.constant(0, dtype=tf.float32))
-        return tf.linalg.norm(perturbation) + c_val * g_func
+        second = softmax[target_label_index]
+        g_func = first - second
+        return tf.square(tf.linalg.norm(delta-image)) + c_val * g_func
 
     for i in range(i_max):
         AdamOptimizer().minimize(loss, var_list=[perturbation])
-        # if i%100==0: show_plot(model(image + perturbation), image + perturbation, model.get_label_names())
+        # if i%500==0:
+        #     delta = (0.5 * (tf.tanh(perturbation) + 1))
+        #     show_plot(model(delta), delta, model.get_label_names())
 
-    return perturbation
-
-    # c_high = c
-    # c_low = 0
-    # for i in range(20):
-    #     c_half = (c_high-c_low)/2
-    #     # perturbation = init_perturbation()
-    #     new_pert = optimize()
-    #     if model(image + perturbation).numpy().squeeze().argmax() == target_label.numpy().squeeze().argmax():
-    #         print("c_high = c_half tested halfing c= " + str(c_half))
-    #         c_high = c_half
-    #     else:
-    #         print("c_low = c_half tested halfing c= " + str(c_half))
-    #         c_low = c_half
-
+    return 0.5 * (tf.tanh(perturbation) + 1)
 
 def c_and_w(data_sample, model, i_max, target_label, min=0.0, max=1.0):
     image, label = data_sample
-    c_high = 100
+    show_plot(model(image), image, model.get_label_names())
+    c_high = 1000
     c_low = 0
     c_half = None
-    while c_high-c_low >= 1e-2:
+    for i in range(20):
         c_half = (c_high+c_low)/2
-        perturbation = optimize_c_and_w(data_sample, model, i_max, target_label, c_half, min=min, max=max)
+        new_image = optimize_c_and_w(data_sample, model, i_max, target_label, c_half, min=min, max=max)
 
-        if model(image + perturbation).numpy().squeeze().argmax() == target_label.numpy().squeeze().argmax():
+        if model(new_image).numpy().squeeze().argmax() == target_label.numpy().squeeze().argmax():
             print("c_high = c_half tested halfing c= " + str(c_half))
             c_high = c_half
+            if c_high - c_low <= 0.01: break
         else:
             print("c_low = c_half tested halfing c= " + str(c_half))
             c_low = c_half
-        show_plot(model(image + perturbation), image + perturbation, model.get_label_names())
+        show_plot(model(new_image), new_image, model.get_label_names())
 
     print("c_half = " + str(c_half))
-    perturbation = optimize_c_and_w(data_sample, model, i_max, target_label, c_half, min=min, max=max)
-    show_plot(model(image + perturbation), image + perturbation, model.get_label_names())
-
-
-
-
-
+    new_image = optimize_c_and_w(data_sample, model, 10 * i_max, target_label, c_half, min=min, max=max)
+    show_plot(model(new_image), new_image, model.get_label_names())
 
 def test_c_and_w():
     model = ConvModel()
     model.load_model_data()
-    target_label = tf.one_hot(tf.constant(6, dtype=tf.int64, shape=(1)), model.get_number_of_classes())
+    target_label = tf.one_hot(tf.constant(3, dtype=tf.int64, shape=(1)), model.get_number_of_classes())
     for data_sample in  model.get_dataset(Split.TEST, batch_size=1).take(1):
-        c_and_w(data_sample, model, 10000, target_label)
+        c_and_w(data_sample, model, 1000, target_label)
+        # optimize_c_and_w(data_sample, model, 10000, target_label, 10)
 
 if __name__ == "__main__":
     enable_eager_execution()
