2d1a473cdc446af94cbb119295ba9b8ca521b8fe
sackhornxd <sackhorn@gmail.com>
2019-05-20 20:49:06 +0200

    made optimizer run faster

--- models/Attacks/c_and_w.py
+++ models/Attacks/c_and_w.py
@@ -1,4 +1,5 @@
 #https://arxiv.org/pdf/1312.6199.pdf opis metody + są jeszcze dwie kolejne publikacje tych samych autorów
+import time
 
 from scipy.optimize import fmin_l_bfgs_b
 from tensorflow.python import enable_eager_execution
@@ -34,24 +35,28 @@ def optimize_c_and_w(data_sample, model, i_max, target_label, c_val, min=0.0, ma
         g_func = first - second
         return tf.square(tf.linalg.norm(delta-image)) + c_val * g_func
 
+    optimizer = AdamOptimizer(epsilon=1e-2)
     for i in range(i_max):
-        AdamOptimizer().minimize(loss, var_list=[perturbation])
-        # if i%500==0:
-        #     delta = (0.5 * (tf.tanh(perturbation) + 1))
-        #     show_plot(model(delta), delta, model.get_label_names())
+        with tf.GradientTape() as tape:
+            tape.watch(perturbation)
+            gradient = tape.gradient(loss(), perturbation)
+            optimizer.apply_gradients([(gradient, perturbation)])
 
     return 0.5 * (tf.tanh(perturbation) + 1)
 
 def c_and_w(data_sample, model, i_max, target_label, min=0.0, max=1.0):
     image, label = data_sample
     show_plot(model(image), image, model.get_label_names())
-    c_high = 1000
+    c_high = 100
     c_low = 0
     c_half = None
     for i in range(12):
         c_half = (c_high+c_low)/2
-        new_image = optimize_c_and_w(data_sample, model, i_max, target_label, c_half, min=min, max=max)
 
+        start = time.time()
+        new_image = optimize_c_and_w(data_sample, model, i_max, target_label, c_half, min=min, max=max)
+        end = time.time()
+        print("one calculation took: " + str(end-start))
         if model(new_image).numpy().squeeze().argmax() == target_label.numpy().squeeze().argmax():
             print("c_high = c_half tested halfing c= " + str(c_half))
             c_high = c_half
@@ -62,7 +67,7 @@ def c_and_w(data_sample, model, i_max, target_label, min=0.0, max=1.0):
         show_plot(model(new_image), new_image, model.get_label_names())
 
     print("c_half = " + str(c_half))
-    new_image = optimize_c_and_w(data_sample, model, 10 * i_max, target_label, c_high, min=min, max=max)
+    new_image = optimize_c_and_w(data_sample, model, i_max, target_label, c_high, min=min, max=max)
     show_plot(model(new_image), new_image, model.get_label_names())
 
 def test_c_and_w():
@@ -70,10 +75,13 @@ def test_c_and_w():
     model.load_model_data()
     target_label = tf.one_hot(tf.constant(6, dtype=tf.int64, shape=(1)), model.get_number_of_classes())
     for data_sample in  model.get_dataset(Split.TEST, batch_size=1).take(1):
-        c_and_w(data_sample, model, 1000, target_label)
+        c_and_w(data_sample, model, 10000, target_label)
         # optimize_c_and_w(data_sample, model, 10000, target_label, 10)
 
 if __name__ == "__main__":
     enable_eager_execution()
     for i in range(1):
+        start = time.time()
         test_c_and_w()
+        end = time.time()
+        print("calculations took: " + str(end-start))
