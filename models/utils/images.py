a00e40dd752b691a0d5604f34592cd47e556ae73
sackhornxd <sackhorn@gmail.com>
2019-08-04 18:35:07 +0200

    top-k labeling of graphs is now possible

--- models/utils/images.py
+++ models/utils/images.py
@@ -8,9 +8,18 @@ def show_plot(logits, image, labels_names=None):
     :type logits: Tensor
     :type image: Tensor
     """
-    labels_names = np.arange(logits.numpy().size) if labels_names is None else labels_names
+    top_k_display = labels_names is not None and len(logits.numpy().squeeze()) > 15
+    labels_names = np.arange(logits.numpy().size) if labels_names is None else np.array(labels_names)
+    probs = logits.numpy().flatten()
+    if top_k_display:
+        sorted_probs_idx = np.argsort(probs)[-10:]
+        sorted_probs = probs.take(sorted_probs_idx)
+        probs = sorted_probs
+        labels_names = labels_names.take(sorted_probs_idx)
+    probs = probs.tolist()
+    labels_names = labels_names.tolist()
 
-    probs = logits.numpy().flatten().tolist()
+
 
     plt.subplot(1, 2, 1)
     plt.imshow(tf.squeeze(image), cmap=plt.get_cmap("gray"))
