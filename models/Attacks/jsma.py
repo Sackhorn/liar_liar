402921de2655f5ca36e56c52acbaf3f4cad30db4
sackhornxd <sackhorn@gmail.com>
2019-08-26 06:19:06 +0200

    logits, un-targeted and decreasing JSMA

--- models/Attacks/jsma.py
+++ models/Attacks/jsma.py
@@ -11,86 +11,109 @@ import tensorflow_datasets as tfds
 # Source https://arxiv.org/pdf/1511.07528.pdf
 
 
-def jsma(data_sample, model, i, eps, target_label, min=0.0, max=1.0):
+def jsma(data_sample,
+         model,
+         i_max,
+         theta,
+         target_label=-1,
+         is_increasing=True,
+         min=0.0,
+         max=1.0,
+         show_plots=False,
+         use_logits=False):
+
+    is_targeted = target_label != -1
     image, true_label = data_sample
-
     true_label = true_label.numpy().squeeze().argmax()
-    current_prediction = true_label
+    current_prediction = model(image).numpy().squeeze().argmax()
+    if true_label != current_prediction:
+        raise ValueError("The image given was wrongly classified in the first place")
 
-    show_plot(model(image), image, model.get_label_names())
     input_shape = model.get_input_shape()
-
     all_pixels = generate_all_pixels(input_shape)
-
+    all_pixels = prune_saturated_pixels(all_pixels, image, is_increasing, min, max)
     iter = 0
-    while iter < i and len(all_pixels) > 0 and current_prediction != target_label:
-        all_pixels_pairs = generate_all_pixel_pairs(all_pixels)
-        chosen_pixel_pair = saliency_map(model, image, true_label, target_label, all_pixels_pairs)
-        add_tensor = np.zeros(input_shape)
-        for pixel in chosen_pixel_pair:
-            if input_shape[2] > 1:
-                add_tensor[pixel[0]][pixel[1]][pixel[2]] = eps
-                pixel_val = image.numpy()[0][pixel[0]][pixel[1]][pixel[2]] + eps
-            else:
-                add_tensor[pixel[0]][pixel[1]] = eps
-                pixel_val = image.numpy()[0][pixel[0]][pixel[1]] + eps
 
-            if pixel_val < 1e-10 or pixel_val > 0.99:
-                all_pixels.remove(pixel)
+    if show_plots:
+        show_plot(model(image), image, labels_names=model.get_label_names())
+
+    # this function determines whether the algorithm should end work
+    def has_met_target(is_targeted, target_label, true_label, current_prediction):
+        if is_targeted:
+            return current_prediction == target_label
+        else:
+            return current_prediction != true_label
+
+    while iter < i_max and len(all_pixels) > 0 and not has_met_target(is_targeted, target_label, true_label, current_prediction):
+        chosen_pixel_pair, theta_sign = saliency_map(model,
+                                                     image,
+                                                     true_label,
+                                                     target_label,
+                                                     all_pixels,
+                                                     is_targeted,
+                                                     is_increasing,
+                                                     use_logits)
+        add_tensor = np.zeros(input_shape)
+        first, second = chosen_pixel_pair
+        add_tensor[first] = theta * theta_sign
+        add_tensor[second] = theta * theta_sign
 
         add_tensor = add_tensor.reshape(input_shape)
         add_tensor = tf.convert_to_tensor(add_tensor, dtype=tf.float32)
-        tf.reshape(add_tensor, input_shape)
-        image = image + add_tensor
-        image = tf.clip_by_value(image, min, max)
-        current_prediction = model(image)
-        # show_plot(current_prediction, image, model.get_label_names())
-        current_prediction = current_prediction.numpy().squeeze().argmax()
-        iter += 1
+        image = tf.clip_by_value(image + add_tensor, min, max)
 
-    show_plot(model(image), image, model.get_label_names())
+        current_prediction = model(image).numpy().squeeze().argmax()
+        all_pixels = prune_saturated_pixels(all_pixels, image, is_increasing, min, max)
+        iter += 1
+        if show_plots:
+            show_plot(model(image), image, labels_names=model.get_label_names())
+            print("iteration: " + str(iter))
+
+    return image
 
 # TODO:nie traktować każdej barwy jako osobny ficzer tylko sumować i zmieniać intensywność
-def saliency_map(model, image, true_label, target_label, all_pixel_pairs):
+def saliency_map(model, image, true_label, target_label, all_pixels, is_targeted, is_increasing, use_logits):
+    all_pixel_pairs = generate_all_pixel_pairs(all_pixels)
     nmb_classes = model.get_number_of_classes()
     input_shape = model.get_input_shape()
+
+    # Generate forward derivatives for each of the classes
     grds_by_cls = []
     with tf.GradientTape(persistent=True) as tape:
         tape.watch(image)
-        logits = model(image)
+        logits = model(image, get_raw=use_logits)
         for k in range(nmb_classes):
-            grds_by_cls.append(tf.squeeze(tape.gradient(logits[0][k], image)).numpy())
+            grds_by_cls.append(tape.gradient(logits[0][k], image).numpy().squeeze())
 
+    # initialize before algorithm run
     max = float("-inf")
     return_pixel_pair = None
 
+    # This is important in case of un-targeted attack when we want to consider all classes except the true one
+    # not just the target_class
+    classes_to_iterate_over = [target_label] if is_targeted else list(range(nmb_classes))
 
-    for pixel_pair in all_pixel_pairs:
-        alpha = 0.0
-        beta = 0.0
-        first, second = pixel_pair
+    for considered_class in classes_to_iterate_over:
+        for pixel_pair in all_pixel_pairs:
+            first, second = pixel_pair
+            beta = 0.0
 
-        if input_shape[2] > 1:
-            alpha += grds_by_cls[target_label][first[0]][first[1]][first[2]]
-            alpha += grds_by_cls[target_label][second[0]][second[1]][second[2]]
-        else:
-            alpha += grds_by_cls[target_label][first[0]][first[1]]
-            alpha += grds_by_cls[target_label][second[0]][second[1]]
+            alpha = grds_by_cls[considered_class][first] + grds_by_cls[considered_class][second]
+            for class_nmb in range(nmb_classes):
+                if class_nmb != considered_class:
+                    beta += grds_by_cls[class_nmb][first] + grds_by_cls[class_nmb][second]
 
-        for class_nmb in range(nmb_classes):
-            if class_nmb != target_label:
-                if input_shape[2] > 1:
-                    beta += grds_by_cls[class_nmb][first[0]][first[1]][first[2]]
-                    beta += grds_by_cls[class_nmb][second[0]][second[1]][second[2]]
-                else:
-                    beta += grds_by_cls[class_nmb][first[0]][first[1]]
-                    beta += grds_by_cls[class_nmb][second[0]][second[1]]
+            if (is_increasing and is_targeted) or (not is_increasing and not is_targeted):
+                is_pair_accepted = alpha > 0.0 and beta < 0 and -alpha * beta > max
+            elif not (is_increasing and is_targeted) or (is_increasing and not is_targeted):
+                is_pair_accepted = alpha < 0.0 and beta > 0 and -alpha * beta > max
 
-        if alpha > 0.0 and beta < 0 and -alpha*beta > max:
-            return_pixel_pair = pixel_pair
-            max = -alpha*beta
+            if is_pair_accepted:
+                return_pixel_pair = pixel_pair
+                max = -alpha*beta
 
-    return return_pixel_pair
+    theta_sign = 1 if is_increasing else -1
+    return return_pixel_pair, theta_sign
 
 
 
@@ -99,8 +122,23 @@ def generate_all_pixels(shape):
     all_pixels = []
     for i in range(x):
         for j in range(y):
-            for k in range(z):
-                all_pixels.append((i, j, k))
+            if z > 1:
+                for k in range(z):
+                    all_pixels.append((i, j, k))
+            else:
+                all_pixels.append((i, j))
+    return all_pixels
+
+def prune_saturated_pixels(all_pixels, image, is_increasing, min, max):
+    np_image = image.numpy().squeeze()
+    pixels_to_remove = []
+    for pixel in all_pixels:
+        if is_increasing and (max-np_image[pixel]) < 1e-5:
+            pixels_to_remove.append(pixel)
+        elif not is_increasing and (np_image[pixel]-min) < 1e-5:
+            pixels_to_remove.append(pixel)
+    for pixel in pixels_to_remove:
+        all_pixels.remove(pixel)
     return all_pixels
 
 def generate_all_pixel_pairs(all_pixels):
@@ -112,19 +150,33 @@ def generate_all_pixel_pairs(all_pixels):
             all_pairs.append((first, second))
     return all_pairs
 
+def targeted_jsma(data_sample, model, i_max, eps, target_label, is_increasing=True, min=0.0, max=1.0, use_logits=False, show_plots=False):
+    return jsma(data_sample, model, i_max, eps, target_label=target_label, is_increasing=is_increasing, use_logits=use_logits, min=min, max=max, show_plots=show_plots)
 
-def execute_jsma(data_sample, model, i, eps, target_label,  min=0.0, max=1.0):
-    return jsma(data_sample, model, i, eps, target_label, min=min, max=max)
+def untargeted_jsma(data_sample, model, i_max, eps, is_increasing=True, min=0.0, max=1.0, use_logits=False, show_plots=False):
+    return jsma(data_sample, model, i_max, eps, min=min, max=max, is_increasing=is_increasing, use_logits=use_logits, show_plots=show_plots)
 
-def test_fgsm_mnist():
-    # model = ConvModel().load_model_data()
-    model = DenseModel().load_model_data()
-    eval_dataset = model.get_dataset(tfds.Split.TEST, batch_size=1)
-    for data_sample in eval_dataset.take(5):
-        _, true_label = data_sample
+def test_jsma():
+    # model = DenseModel().load_model_data()
+    model = ConvModel().load_model_data()
+    eval_dataset = model.get_dataset(tfds.Split.TEST, batch_size=1, shuffle=1)
+    # eval_dataset = model.get_dataset(tfds.Split.TEST, batch_size=1)
+    for data_sample in eval_dataset.take(1):
+        image, true_label = data_sample
         target_label = true_label.numpy().squeeze().argmax() + 1 % model.get_number_of_classes()
-        execute_jsma(data_sample, model, 50, 1.0, target_label)
+
+        # show_plot(model(image), image, model.get_label_names())
+        ret_image = untargeted_jsma(data_sample, model, 50, 1.0, is_increasing=True, use_logits=False, show_plots=True)
+        # ret_image = targeted_jsma(data_sample, model, 50, 1.0, target_label, is_increasing=True, use_logits=True, show_plots=True)
+        # show_plot(model(ret_image), ret_image, model.get_label_names())
+
+        # show_plot(model(image), image, model.get_label_names())
+        # ret_image = untargeted_jsma(data_sample, model, 50, 1.0, is_increasing=True, use_logits=False)
+        # ret_image = targeted_jsma(data_sample, model, 50, 1.0, target_label, is_increasing=True, use_logits=False)
+        # show_plot(model(ret_image), ret_image, model.get_label_names())
+
+
 
 if __name__ == "__main__":
     enable_eager_execution()
-    test_fgsm_mnist()
+    test_jsma()
