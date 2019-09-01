16f7c55cb0667f41e789b63e2a9cd9dc0dd72390
sackhornxd <sackhorn@gmail.com>
2019-09-01 09:42:15 +0200

    c_and_w works vectorized

--- models/Attacks/c_and_w.py
+++ models/Attacks/c_and_w.py
@@ -7,22 +7,19 @@ import numpy as np
 from tensorflow.python.training.gradient_descent import GradientDescentOptimizer
 from tensorflow_datasets import Split
 
+from models.Attacks.attack import Attack
 from models.CIFAR10Models.ConvModel import ConvModel
+from models.ImageNet.InceptionV3Wrapper import ResNetWrapper
 from models.MNISTModels.DenseModel import DenseModel
 from models.utils.images import show_plot
-from models.BaseModels.SequentialModel import SequentialModel
 
 
 @tf.function
 def optimize_c_and_w(image, model, i_max, target_label, c_val, perturbation, optimizer):
-    """
-
-    :type model: SequentialModel
-    """
-
-    target_label_index = tf.argmax(target_label, output_type=tf.int32, axis=1)
+    target_label_index = tf.argmax(target_label, output_type=tf.int32, axis=0)
     target_label_index = tf.reshape(target_label_index, [])
-    for i in tf.range(i_max):
+    tf.print(c_val)
+    for _ in tf.range(i_max):
         with tf.GradientTape() as tape:
             tape.watch(perturbation)
             delta = 0.5 * (tf.tanh(perturbation) + 1)
@@ -31,66 +28,57 @@ def optimize_c_and_w(image, model, i_max, target_label, c_val, perturbation, opt
             first = tf.reduce_max(first, axis=1)
             second = softmax[:, target_label_index]
             g_func = first - second
-            loss =  tf.square(tf.linalg.norm(delta - image)) + c_val * g_func
+            loss =  tf.square(tf.linalg.norm(delta - image)) + tf.multiply(c_val, g_func)
         gradient = tape.gradient(loss, perturbation)
         optimizer.apply_gradients([(gradient, perturbation)])
     return 0.5 * (tf.tanh(perturbation) + 1)
 
-def c_and_w(data_sample, model, i_max, target_label, min=0.0, max=1.0):
-    image, label = data_sample
-    show_plot(model(image), image, model.get_label_names())
-    c_high = 100
-    c_low = 0
-    c_half = None
-    for i in range(20):
-        c_half = (c_high+c_low)/2
+@tf.function
+def c_and_w(image, model, target_label, perturbation, optimizer, iter_max=10000, binary_iter_max=10):
+    c_high = tf.fill([image.shape[0]], 100.0)
+    c_low = tf.fill([image.shape[0]], 0.01)
+    prev_high = c_high
 
-        start = time.time()
+    for _ in tf.range(binary_iter_max):
+        c_half = (c_high + c_low) / 2
+        new_image = optimize_c_and_w(image, model, iter_max, target_label, c_half, perturbation, optimizer)
+        output = model(new_image)
+        output = tf.one_hot(tf.argmax(output, axis=1), model.get_number_of_classes())
+        has_succeed = tf.math.reduce_all(tf.math.equal(output, target_label), axis=1)
+        prev_high = c_high
+        c_high = tf.where(has_succeed, c_half, c_high)
+        c_low = tf.where(has_succeed, c_low, c_half)
+        tf.compat.v1.assign(perturbation, tf.random.uniform(perturbation.shape))
 
-        perturbation = tf.Variable(np.random.normal(min, max, image.shape), dtype=tf.float32)
+    return optimize_c_and_w(image, model, iter_max, target_label, prev_high, perturbation, optimizer)
+
+
+
+
+class CarliniWagner(Attack):
+
+    @staticmethod
+    def run_attack(model, data_sample, target_class):
+        image, label = data_sample
+        perturbation = tf.Variable(np.random.uniform(0.0, 1.0, image.shape), dtype=tf.float32)
         optimizer = GradientDescentOptimizer(1e-2)
-        new_image = optimize_c_and_w(image, model, i_max, target_label, tf.constant(c_half, dtype=tf.float32), perturbation, optimizer)
-
-        end = time.time()
-        print("one calculation took: " + str(end-start))
-
-        if model(new_image).numpy().squeeze().argmax() == target_label.numpy().squeeze().argmax():
-            print("c_high = c_half tested halfing c= " + str(c_half))
-            c_high = c_half
-            if c_high - c_low <= 0.01: break
-        else:
-            print("c_low = c_half tested halfing c= " + str(c_half))
-            c_low = c_half
-        show_plot(model(new_image), new_image, model.get_label_names())
-
-    print("c_half = " + str(c_half))
-    perturbation = tf.Variable(np.random.normal(min, max, image.shape), dtype=tf.float32)
-    optimizer = GradientDescentOptimizer(1e-2)
-    new_image = optimize_c_and_w(image, model, i_max, target_label, tf.constant(c_half, dtype=tf.float32), perturbation, optimizer)
-    show_plot(model(new_image), new_image, model.get_label_names())
-
-
-
+        target_label = tf.one_hot(target_class, model.get_number_of_classes())
+        iter_max = 10000
+        image = c_and_w(image, model, target_label, perturbation, optimizer, iter_max)
+        return (image, model(image), 0) #TODO: fix the returns here to be consistent with run_attacks
 
 def test_c_and_w():
-    # model = DenseModel().load_model_data()
+    # model = ResNetWrapper()
     model = ConvModel().load_model_data()
-    target_label = tf.one_hot(tf.constant(1, dtype=tf.int64, shape=(1)), model.get_number_of_classes())
-    batch_size = 10
+    # model = DenseModel().load_model_data()
+    batch_size = 100
     for data_sample in  model.get_dataset(Split.TEST, batch_size=batch_size, shuffle=1).take(1):
-        image, label = data_sample
-        perturbation = tf.Variable(np.random.normal(0.0, 1.0, image.shape), dtype=tf.float32)
-        optimizer = GradientDescentOptimizer(1e-2)
-
-        start = time.time()
-        ret_image = optimize_c_and_w(image, model, 10000, target_label, tf.constant(1, dtype=tf.float32), perturbation, optimizer)
-
-        end = time.time()
-        print("calculations took: " + str(end - start))
-        for i in range(batch_size):
-            show_plot(model(tf.expand_dims(image[i], axis=0)), tf.expand_dims(image[i], axis=0), model.get_label_names())
+        ret_image, ret_labels, _ = CarliniWagner.run_attack(model, data_sample, 1)
+        for i in range(int(batch_size/10)):
+            show_plot(model(tf.expand_dims(data_sample[0][i], axis=0)), tf.expand_dims(data_sample[0][i], axis=0), model.get_label_names())
             show_plot(model(tf.expand_dims(ret_image[i], axis=0)), tf.expand_dims(ret_image[i], axis=0), model.get_label_names())
 
 if __name__ == "__main__":
-
+    start = time.time()
     test_c_and_w()
+    print("CW took: " + str(time.time() - start))
