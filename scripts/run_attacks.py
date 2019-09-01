0437bb007487b06868aef874e8dd0152653c0cd0
sackhornxd <sackhorn@gmail.com>
2019-09-01 04:54:51 +0200

    attacks deployment now slightly better

--- scripts/run_attacks.py
+++ scripts/run_attacks.py
@@ -1,21 +1,13 @@
 import time
-from multiprocessing.pool import Pool
-
-import tensorflow as tf
 import tensorflow_datasets as tfds
+
+from multiprocessing.pool import Pool
 from math import floor
-
-from models.Attacks.jsma import jsma, jsma_plus_increasing
-
-import models.BaseModels.DataProvider
+from models.Attacks.jsma import jsma_plus_increasing
 from models.CIFAR10Models.ConvModel import ConvModel
 
 
 def run_targeted_atacks(attack, model, dataset_percentage):
-    """
-
-    :type model: models.BaseModels.SequentialModel
-    """
     succesful_attacks = 0
     dataset = model.get_dataset(tfds.Split.TEST, batch_size=1, shuffle=1)
     test_samples_nmb = floor(model.get_info().splits['test'].num_examples * dataset_percentage)
@@ -31,43 +23,36 @@ def run_targeted_atacks(attack, model, dataset_percentage):
         print(succesful_attacks)
         print("it took: " + str(time.time() - start))
     failed_attacks = test_samples_nmb - succesful_attacks
-    # print("success_ratio is {0.2f}".format((succesful_attacks/test_samples_nmb)*100))
     return succesful_attacks, failed_attacks
 
-def run_targeted_atacks_multiproccess(model, dataset_percentage):
-    """
-
-    :type model: models.BaseModels.SequentialModel
-    """
-
+def run_atacks_multiproccess(attack, model, dataset_percentage, target_class=None):
     dataset = model.get_dataset(tfds.Split.TEST, batch_size=1, shuffle=1)
     test_samples_nmb = floor(model.get_info().splits['test'].num_examples * dataset_percentage)
+    attack = wrap_attack_for_multiprocess(attack, model, target_class)
+    with Pool(32) as p:
+        ret_array = p.map(attack, dataset.take(test_samples_nmb))
 
-    # for data_sample in dataset.take(test_samples_nmb):
-
-    with Pool(24) as p:
-        print(p.map(run_fast_jsma, dataset.take(test_samples_nmb)))
+    print("successfully attacked " + str(ret_array.count(True)) + " out of " + str(test_samples_nmb - ret_array.count(None)))
+    print("win ration is " + str((ret_array.count(True)/(test_samples_nmb - ret_array.count(None))) * 100.0) )
 
 
-def run_fast_jsma(data_sample):
-    start = time.time()
-    model = ConvModel().load_model_data()
-    image, true_class = data_sample
-    true_class = true_class.numpy().squeeze().argmax()
-    target_class = true_class - 1 % model.get_number_of_classes()
-    if true_class != model(image).numpy().squeeze().argmax():
-        return False
-    _, return_class = jsma_plus_increasing().run_attack(model, data_sample, target_class)
-    print(return_class == target_class)
-    print("it took: " + str(time.time() - start))
-    return return_class == target_class
+def wrap_attack_for_multiprocess(attack, model, target_class=None):
+    def wrapper(data_sample):
+        start = time.time()
+        image, true_class = data_sample
+        classified_class = model(image).numpy().squeeze().argmax()
+        true_class = true_class.numpy().squeeze().argmax()
+        # this covers a case when dataset gives us a misclassified example or one that agrees with target_class
+        if true_class != classified_class or true_class == target_class:
+            return (None, -1.0, -1) # (was_attack_successful, time_it_took, nmb_iterations)
+        _, return_class, iterations = attack().run_attack(model, data_sample, target_class)
+        duration = time.time() - start
+        was_successful = return_class == target_class
+        return (was_successful, duration, iterations)
 
+    return wrapper
 
 if __name__ == "__main__":
-    start = time.time()
-    run_targeted_atacks_multiproccess(ConvModel().load_model_data(), 0.01)
-    print("multiprocess took " + str(time.time() - start))
 
     start = time.time()
-    run_targeted_atacks(jsma_plus_increasing, ConvModel().load_model_data(), 0.01)
-    print("Single process took" + str(time.time() - start))
+    run_atacks_multiproccess(jsma_plus_increasing, ConvModel().load_model_data(), 1.0, target_class=0)
