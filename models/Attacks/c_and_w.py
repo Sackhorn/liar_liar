6e64b6055183a6001a411f107942b2e5245cb6e3
sackhornxd <sackhorn@gmail.com>
2019-09-01 04:54:08 +0200

    carlini and wagner now as a tf.function

--- models/Attacks/c_and_w.py
+++ models/Attacks/c_and_w.py
@@ -1,23 +1,17 @@
 #https://arxiv.org/pdf/1312.6199.pdf opis metody + są jeszcze dwie kolejne publikacje tych samych autorów
-import time
 
-from scipy.optimize import fmin_l_bfgs_b
-from tensorflow.python import enable_eager_execution
-from tensorflow.python.training.adam import AdamOptimizer
+import time
+import tensorflow as tf
+import numpy as np
+
 from tensorflow.python.training.gradient_descent import GradientDescentOptimizer
-from tensorflow.python.training.momentum import MomentumOptimizer
-from tensorflow.python.training.rmsprop import RMSPropOptimizer
 from tensorflow_datasets import Split
 
 from models.CIFAR10Models.ConvModel import ConvModel
-from models.ImageNet.InceptionV3Wrapper import ResNetWrapper
 from models.MNISTModels.DenseModel import DenseModel
-from models.MNISTModels.LeNet5 import LeNet5
 from models.utils.images import show_plot
 from models.BaseModels.SequentialModel import SequentialModel
 
-import tensorflow as tf
-import numpy as np
 
 @tf.function
 def optimize_c_and_w(image, model, i_max, target_label, c_val, perturbation, optimizer):
@@ -33,9 +27,9 @@ def optimize_c_and_w(image, model, i_max, target_label, c_val, perturbation, opt
             tape.watch(perturbation)
             delta = 0.5 * (tf.tanh(perturbation) + 1)
             softmax = tf.squeeze(model(delta, get_raw=True))
-            first = tf.concat([softmax[:target_label_index], softmax[target_label_index + 1:]], 0)  # get all elements except of the one with highest probability
-            first = tf.reduce_max(first)
-            second = softmax[target_label_index]
+            first = tf.concat([softmax[:, :target_label_index], softmax[:, target_label_index + 1:]], 1)  # get all elements except of the one with highest probability
+            first = tf.reduce_max(first, axis=1)
+            second = softmax[:, target_label_index]
             g_func = first - second
             loss =  tf.square(tf.linalg.norm(delta - image)) + c_val * g_func
         gradient = tape.gradient(loss, perturbation)
@@ -76,26 +70,26 @@ def c_and_w(data_sample, model, i_max, target_label, min=0.0, max=1.0):
     show_plot(model(new_image), new_image, model.get_label_names())
 
 
-def test_c_and_w():
-    model = DenseModel().load_model_data()
-    # model = ConvModel().load_model_data()
-    target_label = tf.one_hot(tf.constant(0, dtype=tf.int64, shape=(1)), model.get_number_of_classes())
 
-    for data_sample in  model.get_dataset(Split.TEST, batch_size=1, shuffle=1).take(10):
+
+def test_c_and_w():
+    # model = DenseModel().load_model_data()
+    model = ConvModel().load_model_data()
+    target_label = tf.one_hot(tf.constant(1, dtype=tf.int64, shape=(1)), model.get_number_of_classes())
+    batch_size = 10
+    for data_sample in  model.get_dataset(Split.TEST, batch_size=batch_size, shuffle=1).take(1):
         image, label = data_sample
         perturbation = tf.Variable(np.random.normal(0.0, 1.0, image.shape), dtype=tf.float32)
         optimizer = GradientDescentOptimizer(1e-2)
 
-        show_plot(model(image), image, model.get_label_names())
         start = time.time()
-        # if tf.test.is_gpu_available():
-        # with tf.device("/cpu:0"):
-        #     image = optimize_c_and_w(image, model, 5000, target_label, tf.constant(1, dtype=tf.float32), perturbation, optimizer)
-        image = optimize_c_and_w(image, model, 5000, target_label, tf.constant(1, dtype=tf.float32), perturbation, optimizer)
+        ret_image = optimize_c_and_w(image, model, 10000, target_label, tf.constant(1, dtype=tf.float32), perturbation, optimizer)
 
         end = time.time()
         print("calculations took: " + str(end - start))
-        show_plot(model(image), image, model.get_label_names())
+        for i in range(batch_size):
+            show_plot(model(tf.expand_dims(image[i], axis=0)), tf.expand_dims(image[i], axis=0), model.get_label_names())
+            show_plot(model(tf.expand_dims(ret_image[i], axis=0)), tf.expand_dims(ret_image[i], axis=0), model.get_label_names())
 
 if __name__ == "__main__":
 
