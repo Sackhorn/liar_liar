8e484cc32562fa2f8af3488160a005fab485c1c2
sackhornxd <sackhorn@gmail.com>
2019-12-14 10:22:54 +0100

    Improved EA, and added LeNet5 definition

--- models/Attacks/evolutionary_attack.py
+++ models/Attacks/evolutionary_attack.py
@@ -1,13 +1,9 @@
 import random
-
 import numpy as np
 from tensorflow.python import enable_eager_execution
-import matplotlib.pyplot as plt
-from tensorflow_datasets import Split
-
-from models.MNISTModels.ConvModel import ConvModel
-from models.MNISTModels.DenseModel import DenseModel
 from models.BaseModels.SequentialModel import SequentialModel
+import matplotlib.pyplot as plt
+
 
 import tensorflow as tf
 
@@ -15,54 +11,61 @@ import tensorflow as tf
 # https://arxiv.org/pdf/1412.1897.pdf
 # https://arxiv.org/pdf/1504.04909.pdf - tutaj jest opisana strategia ewolucyjna
 # https://pdfs.semanticscholar.org/3461/99d0c8fce4bd91be855155bb1fe71844e9ac.pdf - polynomial mutation operator
+from models.CIFAR10Models.ConvModel import ConvModel
 from models.MNISTModels.LeNet5 import LeNet5
 from models.utils.images import show_plot
 
 
-def evolutionary_attack(model, population_size, iter_max, min=0.0, max=1.0, mutation_chance=0.1, mutation_power=20, use_normal=False, use_gausian=False):
+def evolutionary_attack(model, population_size, iter_max, min=0.0, max=1.0, mutation_chance=0.1, mutation_power=15, use_normal=False, use_gausian=False):
     """
     :type model: SequentialModel
     """
-    population = [] #
-    best_in_class = {} #dictionary of specimen performing best for given class
+    # population = [] #
 
-    for x in range(population_size):
-        if use_normal:
-            population.append(np.random.normal(0.0, 0.3, model.get_input_shape())) #initialize specimens with random values
-        else:
-            population.append(np.random.uniform(min, max, model.get_input_shape())) #initialize specimens with random values
+    # best_in_class = {} #dictionary of specimen performing best for given class
+
+    performance_map = {}
+    specimen_map = {}
+    initial_population_size = 1000 * population_size #TODO Ensure that the whole population is initialized or assert that we don't need it in eval_and_insert
+
+    for x in range(initial_population_size):
+        new_specimen = np.random.uniform(min, max, model.get_input_shape()) #initialize specimens with random values
+        eval_and_insert(performance_map, specimen_map, model, new_specimen)
+
 
     for i in range(iter_max):
-        if i%100==0: print(i)
-        if i%500==0: mutation_chance = mutation_chance * 0.9
-        current_specimen = random.choice(population)
-        if use_gausian:
-            mutated_specimen = mutate_specimen_gausian(current_specimen, min, max, mutation_chance)
-        else:
-            mutated_specimen = mutate_specimen(current_specimen, min, max, mutation_chance, mutation_power)
+        if i%100==0: print("generation: " + str(i))
+        if i%2000==0: mutation_chance = mutation_chance * 0.5
 
-        logits = model(tf.expand_dims(tf.convert_to_tensor(mutated_specimen, dtype=tf.float32), 0))
+        current_specimen = random.choice(list(specimen_map.values()))
+        mutated_specimen = [mutate_specimen(current_specimen, min, max, mutation_chance, mutation_power=mutation_power) for i in range(2*population_size)]
+        for specimen in mutated_specimen:
+            eval_and_insert(performance_map, specimen_map, model, specimen)
 
-        certainties_per_class = tf.squeeze(logits).numpy()
-
-        for cur_class in range(len(certainties_per_class)):
-            if cur_class in best_in_class:
-                _, best_certainty = best_in_class[cur_class]
-                if certainties_per_class[cur_class] > best_certainty:
-                    best_in_class[cur_class] = (mutated_specimen, certainties_per_class[cur_class])
-                    population.append(mutated_specimen)
-            else:
-                best_in_class[cur_class] = (mutated_specimen, certainties_per_class[cur_class])
-
-    for _, specimen_tuple in best_in_class.items():
-        specimen, _ = specimen_tuple
+    for cur_class, specimen in specimen_map.items():
         specimen = tf.expand_dims(tf.convert_to_tensor(specimen, dtype=tf.float32), 0)
         logits = model(specimen)
         show_plot(logits, specimen, model.get_label_names())
 
-def mutate_specimen(specimen, min, max, mutation_chance, mutation_power=15):
-    specimen = np.copy(specimen)
-    with np.nditer(specimen, op_flags=['readwrite']) as it:
+def eval_and_insert(performance_map, specimen_map, model, specimen):
+    cur_performance = eval_specimen(specimen, model)
+    for idx, performance in enumerate(cur_performance):
+        if idx in performance_map:
+            if cur_performance[idx] > performance_map[idx]:
+                performance_map[idx] = cur_performance[idx]
+                specimen_map[idx] = specimen
+        else:
+            performance_map[idx] = cur_performance[idx]
+            specimen_map[idx] = specimen
+
+
+def eval_specimen(specimen, model):
+    logits = model(tf.expand_dims(tf.convert_to_tensor(specimen, dtype=tf.float32), 0))
+    return tf.squeeze(logits).numpy()
+
+def mutate_specimen(specimen, min, max, mutation_chance, mutation_power):
+    new_specimen = np.copy(specimen)
+    with np.nditer(new_specimen, op_flags=['readwrite']) as it:
         for old_val in it:
             if np.random.uniform() < mutation_chance:
                 u = np.random.uniform()
@@ -73,11 +76,11 @@ def mutate_specimen(specimen, min, max, mutation_chance, mutation_power=15):
                     delta = 1.0 - (2 * (1 - u))** (1 / (1 + mutation_power))
                     new_val = old_val + delta * (max - old_val)
                 old_val[...] = new_val
-    return np.clip(specimen, min, max)
+    return np.clip(new_specimen, min, max)
 
 def mutate_specimen_gausian(specimen, min, max, mutation_chance):
     specimen = np.copy(specimen)
-    std_dev = 0.5 * (max-min)
+    std_dev = 0.25 * (max-min)
     with np.nditer(specimen, op_flags=['readwrite']) as it:
         for old_val in it:
             if np.random.uniform() < mutation_chance:
@@ -87,10 +90,11 @@ def mutate_specimen_gausian(specimen, min, max, mutation_chance):
 
 
 def test_ea_attack():
-    model = DenseModel()
-    model.load_model_data()
-    model.test()
-    evolutionary_attack(model, 1000, 5000, use_normal=False, use_gausian=False)
+    # model = DenseModel().load_model_data()
+    # model = ConvModel().load_model_data()
+    model = LeNet5().load_model_data()
+    # model.test()
+    evolutionary_attack(model, model.get_number_of_classes(), 10000, mutation_power=15, mutation_chance=0.5, use_normal=False, use_gausian=False)
 
 
 if __name__ == "__main__":
--- models/MNISTModels/LeNet5.py
+++ models/MNISTModels/LeNet5.py
@@ -3,20 +3,19 @@ from tensorflow.python.keras.layers import Flatten, Dense, Conv2D, AveragePoolin
 from tensorflow.python.keras.losses import categorical_crossentropy
 from tensorflow.python.keras.metrics import categorical_accuracy
 from tensorflow.python.keras.optimizer_v2.adam import Adam
-from tensorflow.python.keras.optimizers import SGD
 
 from models.MNISTModels.MNISTModel import MNISTModel
 
 
 class LeNet5(MNISTModel):
 
-    def __init__(self, optimizer=SGD(), loss=categorical_crossentropy, metrics=[categorical_accuracy]):
+    def __init__(self, optimizer="sgd", loss=categorical_crossentropy, metrics=[categorical_accuracy]):
         super().__init__(optimizer=optimizer, loss=loss, metrics=metrics, MODEL_NAME="mnist_dense_model")
         self.sequential_layers = [
             Conv2D(6, [5, 5], activation="tanh"),
-            AveragePooling2D(strides=(1,1)),
+            AveragePooling2D(strides=1),
             Conv2D(16, [5, 5], activation="tanh"),
-            AveragePooling2D(strides=(2,2)),
+            AveragePooling2D(),
             Conv2D(120, [5, 5], activation="tanh"),
             Flatten(),
             Dense(84, activation="tanh"),
@@ -27,4 +26,4 @@ class LeNet5(MNISTModel):
 if __name__ == "__main__":
     enable_eager_execution()
     model = LeNet5()
-    model.train(epochs=10)
+    model.train(epochs=15, augment_data=False)
