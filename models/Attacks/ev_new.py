bbe387f290091a823d1e52a35bc4a782debe8ca4
sackhornxd <sackhorn@gmail.com>
2019-12-17 01:17:24 +0100

    improved the new evolutionary attack

--- models/Attacks/ev_new.py
+++ models/Attacks/ev_new.py
@@ -5,25 +5,27 @@ from tensorflow.python import enable_eager_execution
 from tensorflow_datasets import Split
 
 from models.CIFAR10Models.ConvModel import ConvModel
+from models.ImageNet.InceptionV3Wrapper import ResNetWrapper
 from models.MNISTModels.DenseModel import DenseModel
 from models.MNISTModels.LeNet5 import LeNet5
 from models.utils.images import show_plot
 
 # TODO: Generalize for all models
 # https://arxiv.org/pdf/1805.11090.pdf Strategia ewolucyjna
+from models.utils.utils import count
 
 
-
-def evolutionary_attack(model, data_sample, target_class, generation_nmb=500, population_nmb=100, min=0.0, max=1.0, mutation_chance=0.3):
+def evolutionary_attack(model, data_sample, target_class, generation_nmb=1000, population_nmb=10, min=0.0, max=1.0, mutation_chance=0.3):
     """
     :type model: SequentialModel
     """
 
     image, label = data_sample
-    old_population = [mutate_specimen(image.numpy(), min, max, mutation_chance) for i in range(population_nmb)] #creating the initial population
-
+    np_image = image.numpy()
+    shape = np_image.shape
+    old_population = [(np_image + np.random.uniform(-0.05, 0.05, shape)).astype(np.float32) for _ in range(population_nmb)] #creating the initial population
     for k in range(generation_nmb):
-        print(k)
+
         fitness = list(map(lambda x: eval_specimen(target_class, x, model, get_raw=False), old_population))
         highest_scoring_idx = np.argmax(fitness)
         classification = get_classification(old_population[highest_scoring_idx], model)
@@ -36,16 +38,20 @@ def evolutionary_attack(model, data_sample, target_class, generation_nmb=500, po
         else:
             new_population = [old_population[highest_scoring_idx]]
             fitness = list(map(lambda x: eval_specimen(target_class, x, model, get_raw=False), old_population))
+            # print("gen: " + str(k) + " fitness: " + str(np.array(fitness).max()))
+            fitness = tf.nn.softmax(fitness)
+            fitness = fitness.numpy()
 
         for _ in range(len(old_population)-1):
             first_parent_idx, second_parent_idx = pick_parents(fitness)
             child = crossover_parents(old_population[first_parent_idx], old_population[second_parent_idx], fitness[first_parent_idx], fitness[second_parent_idx])
             child = mutate_specimen(child, min, max, mutation_chance)
+            child = clip_child(child, image)
             new_population.append(child)
 
         old_population = new_population
 
-    print("run out of generation numbers")
+    # print("run out of generation numbers")
     fitness = list(map(lambda x: eval_specimen(target_class, x, model, get_raw=True), old_population))
     highest_scoring_idx = np.argmax(fitness)
     output = old_population[highest_scoring_idx].reshape(model.get_input_shape())
@@ -53,27 +59,25 @@ def evolutionary_attack(model, data_sample, target_class, generation_nmb=500, po
     output = tf.expand_dims(output, 0)
     return output
 
+def clip_child(child, image):
+    min = image - 0.1
+    max = image + 0.1
+    return tf.clip_by_value(child, min, max).numpy()
+
 def pick_parents(fitness):
-    parents_list = [None, None]
-    tmp = np.array(fitness)
-    normalized_fitness = tmp/tmp.max()
-    for j in range(2):
-        while parents_list[j] is None:
-            probability = np.random.uniform()
-            for i, softmax_prob in enumerate(normalized_fitness):
-                if probability < softmax_prob:
-                    parents_list[j] = i
-                    break
-    return tuple(parents_list)
+
+    first = np.random.choice(np.arange(len(fitness)), p=fitness)
+    second = np.random.choice(np.arange(len(fitness)), p=fitness)
+    return (first, second)
 
 
 def crossover_parents(first, second, first_fitness, second_fitness):
-    prob_on_pick_first = first_fitness/ (first_fitness+second_fitness)
+    prob_on_pick_first = first_fitness / (first_fitness + second_fitness)
     first_copy = np.copy(first)
     second_copy = np.copy(second)
     with np.nditer([first_copy, second_copy], op_flags=['readwrite']) as it:
         for first_val, second_val in it:
-            if not np.random.uniform() < prob_on_pick_first:
+            if np.random.uniform() > prob_on_pick_first:
                 first_val[...] = second_val
     return first_copy
 
@@ -83,7 +87,12 @@ def eval_specimen(target_class, specimen, model, get_raw):
     input = tf.convert_to_tensor(input, dtype=tf.float32)
     input = tf.expand_dims(input, 0)
     logits = model(input, get_raw=get_raw)
-    return tf.squeeze(logits).numpy()[target_class]
+    logits = tf.squeeze(logits).numpy()
+    probs_without_target = np.array([logits[i] for i in range(len(logits)) if i!=target_class ])
+    probs_without_target = probs_without_target[np.nonzero(probs_without_target)] # We prune elements that are so close to zero that it doesnt matter
+    fitness = np.log(logits[target_class]) - np.log(np.sum(probs_without_target))
+    # fitness = np.log(logits[target_class])
+    return fitness
 
 def get_classification(specimen, model):
     input = specimen.reshape(model.get_input_shape())
@@ -94,7 +103,7 @@ def get_classification(specimen, model):
 
 def mutate_specimen(specimen, min, max, mutation_chance):
     new_specimen = np.copy(specimen)
-    delta = 0.1
+    delta = 0.05
     with np.nditer(new_specimen, op_flags=['readwrite']) as it:
         for old_val in it:
             if np.random.uniform() <= mutation_chance:
@@ -103,14 +112,25 @@ def mutate_specimen(specimen, min, max, mutation_chance):
     return np.clip(new_specimen, min, max)
 
 def test_ea_attack():
-    model = LeNet5().load_model_data()
+    # model = LeNet5().load_model_data()
     model = ConvModel().load_model_data()
-    for data_sample in model.get_dataset(Split.TEST, batch_size=1, augment_data=False).take(10):
+    # model = ResNetWrapper().load_model_data()
+    count_arr = []
+    target_class = 9
+    for data_sample in model.get_dataset(Split.TEST, batch_size=1, augment_data=False):
         image, label = data_sample
         # model(image)
-        show_plot(model(image), image, labels_names=model.get_label_names())
-        ret_image = evolutionary_attack(model, data_sample, target_class=1, mutation_chance=0.1)
-        show_plot(model(ret_image), ret_image, labels_names=model.get_label_names())
+        # show_plot(model(image), image, labels_names=model.get_label_names())
+        ret_image = evolutionary_attack(model, data_sample, target_class=target_class, mutation_chance=0.05)
+        # show_plot(model(ret_image), ret_image, labels_names=model.get_label_names())
+        if tf.argmax(model(ret_image), axis=1) == target_class:
+            count_ret = 1.0
+        else:
+            count_ret = 0.0
+        print(count_ret)
+        count_arr.append(count_ret)
+        avg = np.average(np.array(count_arr))
+        print("average: " + str(avg))
 
 if __name__ == "__main__":
     enable_eager_execution()
