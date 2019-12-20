b42f24a52a54f0f49e0c9c1b0b0fc55a302398d7
sackhornxd <sackhorn@gmail.com>
2019-12-20 05:55:04 +0100

    iterated on AdvGan

--- models/MNISTModels/AdvGANModel.py
+++ models/MNISTModels/AdvGANModel.py
@@ -3,14 +3,17 @@ import time
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from tensorflow.python.keras.layers import Dense
-from tensorflow.python.keras.losses import categorical_crossentropy, BinaryCrossentropy
+from tensorflow.python.keras.losses import categorical_crossentropy, BinaryCrossentropy, CategoricalCrossentropy
 from tensorflow.python.keras.metrics import categorical_accuracy
 from tensorflow.python.keras.optimizer_v2.adam import Adam
 from tensorflow_core.python.keras.layers import BatchNormalization, LeakyReLU, Reshape, Conv2DTranspose, Conv2D, \
     Dropout, Flatten
 from tensorflow_datasets import Split
 
+from models.BaseModels.SequentialModel import SequentialModel
+from models.MNISTModels.ConvModel import ConvModel
 from models.MNISTModels.MNISTModel import MNISTModel
+import numpy as np
 from models.utils.images import show_plot
 
 
@@ -19,7 +22,8 @@ class MNISTGenerator(MNISTModel):
     def __init__(self, optimizer=Adam(1e-4), loss=categorical_crossentropy, metrics=[categorical_accuracy]):
         super().__init__(optimizer=optimizer, loss=loss, metrics=metrics, MODEL_NAME="mnist_generator")
         self.sequential_layers = [
-        Dense(7 * 7 * 256, use_bias=False, input_shape=(100,)),
+        Flatten(),
+        Dense(7 * 7 * 256, use_bias=False),
         BatchNormalization(),
         LeakyReLU(),
         Reshape((7, 7, 256)),
@@ -32,6 +36,10 @@ class MNISTGenerator(MNISTModel):
         Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'),
         ]
         self.gan_loss = BinaryCrossentropy(from_logits=True)
+
+    def call(self, input, get_raw=False, train=False, **kwargs):
+        output = super().call(input, get_raw=get_raw)
+        return output + input
 
     def generator_loss(self, fake_output):
         return self.gan_loss(tf.ones_like(fake_output), fake_output)
@@ -61,34 +69,36 @@ class MNISTDiscriminator(MNISTModel):
 
 generator = MNISTGenerator()
 discriminator = MNISTDiscriminator()
-generator.trainable = True
-discriminator.trainable = True
-# output = generator(tf.random.uniform([1, 100]))
-# logits = discriminator(output)
-# show_plot(logits, output)
+
 
 EPOCHS = 200
-BATCH_SIZE = 256
+BATCH_SIZE = 250
 noise_dim = 100
 num_examples_to_generate = 16
-seed = tf.random.normal([num_examples_to_generate, noise_dim])
-checkpoint = tf.train.Checkpoint(generator_optimizer=generator.optimizer,
-                                 discriminator_optimizer=discriminator.optimizer,
-                                 generator=generator,
-                                 discriminator=discriminator)
 
-@tf.function
+
+classifier: SequentialModel = ConvModel().load_model_data()
+classifier.trainable = False
+seed =  None
+for data in classifier.get_dataset(Split.TRAIN, batch_size=4).take(1):
+    seed, _ = data
+classifier_loss = CategoricalCrossentropy(from_logits=True)
+target_class = tf.one_hot(tf.constant(1, shape=[BATCH_SIZE], dtype=tf.int32), depth=10, dtype=tf.int32)
+generator.trainable = True
+discriminator.trainable = True
+
+# @tf.function
 def train_step(images):
-    noise = tf.random.normal([BATCH_SIZE, noise_dim])
     generator.trainable = True
     discriminator.trainable = True
     with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-      generated_images = generator(noise, train=True)
 
+      generated_images = generator(images, train=True)
       real_output = discriminator(images, train=True)
       fake_output = discriminator(generated_images, train=True)
+      classifier_output = classifier(generated_images, train=False)
 
-      gen_loss = generator.generator_loss(fake_output)
+      gen_loss = generator.generator_loss(fake_output) + 5 * classifier_loss(target_class, classifier_output)
       disc_loss = discriminator.discriminator_loss(real_output, fake_output)
 
     gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
@@ -103,55 +113,40 @@ def train(dataset, epochs):
 
     for image_batch in dataset:
         image, label = image_batch
-        # image = image_batch
         train_step(image)
-
-    # Produce images for the GIF as we go
-    # display.clear_output(wait=True)
-    generate_and_save_images(generator,
-                             epoch + 1,
-                             seed)
-    #
-    # Save the model every 15 epochs
-    # if (epoch + 1) % 15 == 0:
-    #   checkpoint.save(file_prefix = generator.SAVE_DIR)
-
+    generate_and_save_images(generator, epoch + 1, seed)
     print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
 
-  # Generate after the final epoch
-  # display.clear_output(wait=True)
-  generate_and_save_images(generator,
-                           epochs,
-                           seed)
-
 def generate_and_save_images(model, epoch, test_input):
-  # Notice `training` is set to False.
-  # This is so all layers run in inference mode (batchnorm).
   generator.trainable = False
   discriminator.trainable = False
   predictions = model(test_input, train=False)
-  # show_plot(tf.random.uniform([1,10]),predictions[0])
+  probs = classifier(predictions, train=False)
 
   for i in range(predictions.shape[0]):
-      plt.subplot(4, 4, i+1)
-      plt.imshow(predictions[i, :, :, 0], cmap='gray')
-      plt.axis('off')
-
-  # plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
+      plt.subplot(2,4, 2*(i+1)-1)
+      plt.imshow(tf.clip_by_value(predictions[i, :, :, 0], 0.0, 1.0), cmap='gray')
+      plt.xticks([])
+      plt.yticks([])
+      plt.subplot(2,4,2*(i+1))
+      plt.bar(np.arange(len(probs[i])), probs[i])
+      plt.xticks(np.arange(len(probs[i])), np.arange(probs[i].numpy().size), rotation=90)
   plt.show()
+  # plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
 
+def train_gan():
+    dataset = generator.get_dataset(Split.TRAIN, batch_size=BATCH_SIZE, augment_data=False)
+    train(dataset, EPOCHS)
+    generator.save_model_data()
+    discriminator.save_model_data()
 
-dataset = generator.get_dataset(Split.TRAIN, batch_size=256, augment_data=False)
-# checkpoint.restore(tf.train.latest_checkpoint(generator.SAVE_DIR))
-# generator.load_model_data()
-# discriminator.load_model_data()
-train(dataset, EPOCHS)
-generator.save_model_data()
-discriminator.save_model_data()
-#
-#
-# generator.load_model_data()
-# discriminator.load_model_data()
-# for i in range(10):
-#     seed = tf.random.normal([num_examples_to_generate, noise_dim])
-#     generate_and_save_images(generator, 0, seed)
+def test_model():
+    generator.load_model_data()
+    discriminator.load_model_data()
+    dataset = generator.get_dataset(Split.TRAIN, batch_size=4, augment_data=False).take(10)
+    for data in dataset:
+        image, _ = data
+        generate_and_save_images(generator, 0, image)
+
+if __name__=="__main__":
+    train_gan()
