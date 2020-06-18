5a7783515de332cbe2410b099e5cdaf7163d904c
sackhorn <sackhorn@gmail.com>
2020-06-18 22:29:39 +0200

    Added CIFAR10 SimpleNet

--- liar_liar/base_models/model_names.py
+++ liar_liar/base_models/model_names.py
@@ -1,6 +1,7 @@
 CIFAR_10_CONV_NAME = "cifar10_conv_model"
 CIFAR_100_CONV_NAME = "cifar100_conv_model"
 SIMPLENET_CIFAR100_NAME = "simplenet_cifar100"
+SIMPLENET_CIFAR10_NAME = "simplenet_cifar10"
 INCEPTION_V3_NAME = "imagenet_v3"
 RESNET_NAME = "resnet50v2"
 MNIST_CONV_NAME = "mnist_conv_model"
