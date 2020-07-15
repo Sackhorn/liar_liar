1f87fa0e0120afb359cc5cf205011a17432051a3
sackhorn <sackhorn@gmail.com>
2020-07-15 20:09:02 +0200

    sanity check teamcity messaging

--- liar_liar/attack_sanity_check/run_sanity_checks.py
+++ liar_liar/attack_sanity_check/run_sanity_checks.py
@@ -1,7 +1,13 @@
 import os
 import unittest
-loader = unittest.TestLoader()
-path = os.path.dirname(__file__)
-tests = loader.discover(path)
-testRunner = unittest.runner.TextTestRunner()
-testRunner.run(tests)
+from teamcity import is_running_under_teamcity
+from teamcity.unittestpy import TeamcityTestRunner
+if __name__ == "__main__":
+    loader = unittest.TestLoader()
+    path = os.path.dirname(__file__)
+    tests = loader.discover(path)
+    if is_running_under_teamcity():
+        testRunner = TeamcityTestRunner()
+    else:
+        testRunner = unittest.runner.TextTestRunner()
+    testRunner.run(tests)
