2e734efb4b27429da56bd3d2b0e20f1812f6a446
sackhorn <sackhorn@gmail.com>
2020-08-07 18:11:12 +0200

    removed calls to teamcity-messages

--- liar_liar/tests/run_sanity_checks.py
+++ liar_liar/tests/run_sanity_checks.py
@@ -1,13 +1,13 @@
 import os
 import unittest
-from teamcity import is_running_under_teamcity
-from teamcity.unittestpy import TeamcityTestRunner
+# from teamcity import is_running_under_teamcity
+# from teamcity.unittestpy import TeamcityTestRunner
 if __name__ == "__main__":
     loader = unittest.TestLoader()
     path = os.path.dirname(__file__)
     tests = loader.discover(path)
-    if is_running_under_teamcity():
-        testRunner = TeamcityTestRunner()
-    else:
-        testRunner = unittest.runner.TextTestRunner()
+    # if is_running_under_teamcity():
+    #     testRunner = TeamcityTestRunner()
+    # else:
+    testRunner = unittest.runner.TextTestRunner()
     testRunner.run(tests)
